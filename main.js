e2d2b2066df85539461fd316cc68268ad6692f9b
unknown <thecanadianbaconstrip@gmail.com>
2020-01-16 17:35:53 -0700

    scene background

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
+scene.background = new THREE.Color( 0x8FBCD4 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
