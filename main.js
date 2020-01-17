df444803deeaeeb548a516c7b925f555fdbf0573
unknown <thecanadianbaconstrip@gmail.com>
2020-01-16 17:50:05 -0700

    added init function delocalized variables

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+let scene;
+let camera;
+let renderer;
+let cube;
+
+function init(){
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 scene.background = new THREE.Color( 0x8FBCD4 );
-var renderer = new THREE.WebGLRenderer();
+renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
+cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.set(0,0,10);
+};
 function keyControls(){
     document.onkeydown = function(e) {
         switch (e.keyCode){
@@ -33,12 +40,12 @@ function keyControls(){
 var animate = function () {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+cube.rotation.x += 0.01;
+   cube.rotation.y += 0.01;
     keyControls();
     renderer.render( scene, camera );
 
     keyControls();
 };
-
+init();
 animate();
