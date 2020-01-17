2a68b6b6ecdd83b75ef8a7645f8deb51c2f86620
unknown <thecanadianbaconstrip@gmail.com>
2020-01-16 19:21:30 -0700

    ground.js

--- ground.js
+++ ground.js
@@ -1,24 +1,42 @@
-class Ground {
-    let floorGeomety;
-    let floormaterial;
-    constructor() {
+class Ground{
+    
+  
+     constructor(x,y,z,i,j,k)
+     {
+        this.origin = new THREE.Vector3(x,y,z);
 
+        this.side   = 2;
+        
 
-
-
-
-    };
-
-    function createGround() {
-
-        // create an area of 2,2,2 cubes and render them
-        floorGeometry = new THREE.BoxGeometry(25, 2, 25);
-        floorMaterial = new THREE.MeshStandardMaterial({
+        this.floorGeometry = new THREE.BoxGeometry(this.side, this.side, this.side);
+        this.floorMaterial = new THREE.MeshStandardMaterial({
             color: 0x00ff0f
         });
-        floor = new THREE.Mesh(floorGeometry, floorMaterial);
-        floor.position.set(-1, -1, -1);
-        scene.add(floor);
+       
+        
 
-    }
-}
+        // create an array of 2,2,2 cubes and render them
+        this.floorArray = new Array(i);
+
+        for( var n =0; n<i; n++)
+        {
+            this.floorArray[this.n] = new Array(j);
+        }
+        
+        for(var n = 0; n <i;n++)
+        {
+            for(var m = 0; m<j; m++)
+            {
+               
+
+                this.floorArray[n][m] = new THREE.Mesh(this.floorGeometry, this.floorMaterial);
+                this.floorArray[n][m].position.set(x +n*this.side ,y,z + m*this.side);
+
+            }
+
+        }
+
+       scene.add(floorArray[n][m]);
+    };
+    
+};
