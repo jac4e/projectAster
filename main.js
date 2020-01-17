7a2069b7a87ae2750ee7433c415b4d2ee220e82a
unknown <thecanadianbaconstrip@gmail.com>
2020-01-16 17:58:08 -0700

    added directional light and changed mesh material

--- main.js
+++ main.js
@@ -5,19 +5,26 @@ let cube;
 
 function init(){
 scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
 scene.background = new THREE.Color( 0x8FBCD4 );
+
+camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.set(0,0,10);
+
+
 renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
 
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.9 );
+scene.add( directionalLight );
+
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
 cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.set(0,0,10);
+
 };
 function keyControls(){
     document.onkeydown = function(e) {
