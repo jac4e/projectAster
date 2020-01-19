c3fd5f2e5ee935e90fc4bf4a5b30b3dd813005d0
JacquesFourie <jacludvic4e@gmail.com>
2020-01-19 03:24:16 -0700

    moved controls to seperate file

---
+++ controls.js
@@ -0,0 +1,76 @@
+class Controls {
+    constructor() {
+        canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
+    }
+
+    connect() {
+        document.addEventListener('keydown', this.keyDown);
+        document.addEventListener('keyup', this.keyUp);
+        document.addEventListener("click", this.mouseClick);
+        document.addEventListener('pointerlockchange', this.pause); //For future use when we implement a pause menu
+    }
+    
+    keyDown(e) {
+        if (p == false) {
+            switch (e.code) {
+                case 'KeyW':
+                    moveForward = true;
+                    break;
+                case 'KeyA':
+                    moveLeft = true;
+                    break;
+                case 'KeyS':
+                    moveBackward = true;
+                    break;
+                case 'KeyD':
+                    moveRight = true;
+                    break;
+                case 'Space':
+                    moveUp = true;
+                    break;
+                case 'ShiftLeft':
+                    moveDown = true;
+                    break;
+            }
+        }
+    }
+
+    keyUp(e) {
+        if (p == false) {
+            switch (e.code) {
+                case 'KeyW':
+                    moveForward = false;
+                    break;
+                case 'KeyA':
+                    moveLeft = false;
+                    break;
+                case 'KeyS':
+                    moveBackward = false;
+                    break;
+                case 'KeyD':
+                    moveRight = false;
+                    break;
+                case 'Space':
+                    moveUp = false;
+                    break;
+                case 'ShiftLeft':
+                    moveDown = false;
+                    break;
+            }
+        }
+    }
+
+    mouseClick(e) {
+        canvas.requestPointerLock()
+        p = false;
+    }
+
+    pause(e) {
+        if (document.pointerLockElement === canvas ||
+            document.mozPointerLockElement === canvas) {
+            p = false;
+        } else {
+            p = true;
+        }
+    }
+}
--- main.js
+++ main.js
@@ -19,92 +19,19 @@ var prevTime = performance.now();
 function init() {
     //Get html5 canvas and set pointer lock references
     canvas = document.querySelector('#game');
-    canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
 
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-   
+
     renderer = new THREE.WebGLRenderer({canvas});
 
     renderer.autoClearColor = false;
 
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    //Mouse and Keyboard control listeners
-    document.addEventListener('keydown', keyDown);
-    document.addEventListener('keyup', keyUp);
-    document.addEventListener("click", mouseClick);
-    document.addEventListener('pointerlockchange', pause); //For future use when we implement a pause menu
-
-    // Functions
-
-    function keyDown(e) {
-        if (p == false) {
-            switch (e.code) {
-                case 'KeyW':
-                    moveForward = true;
-                    break;
-                case 'KeyA':
-                    moveLeft = true;
-                    break;
-                case 'KeyS':
-                    moveBackward = true;
-                    break;
-                case 'KeyD':
-                    moveRight = true;
-                    break;
-                case 'Space':
-                    moveUp = true;
-                    break;
-                case 'ShiftLeft':
-                    moveDown = true;
-                    break;
-            }
-        }
-    }
-
-    function keyUp(e) {
-        if (p == false) {
-            switch (e.code) {
-                case 'KeyW':
-                    moveForward = false;
-                    break;
-                case 'KeyA':
-                    moveLeft = false;
-                    break;
-                case 'KeyS':
-                    moveBackward = false;
-                    break;
-                case 'KeyD':
-                    moveRight = false;
-                    break;
-                case 'Space':
-                    moveUp = false;
-                    break;
-                case 'ShiftLeft':
-                    moveDown = false;
-                    break;
-            }
-        }
-    }
-
-    function mouseClick(e) {
-        canvas.requestPointerLock()
-        p = false;
-    }
-
-    function pause(e) {
-        if (document.pointerLockElement === canvas ||
-            document.mozPointerLockElement === canvas) {
-            p = false;
-        } else {
-            p = true;
-        }
-    }
-
-    //Lighting
-    
+    controls = new Controls();
+    controls.connect();
 
     world = new World();
 
@@ -114,9 +41,9 @@ function init() {
 };
 
 function update() {
-    world.update();
     var delta = (time - prevTime) / 1000;
     player.control(Number(moveRight) - Number(moveLeft), Number(moveUp) - Number(moveDown), Number(moveBackward) - Number(moveForward), delta);
+    world.update();
     prevTime = time
 }
 
