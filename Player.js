09fc108668e476959f0bcc28bd581794833bb49b
Jacques Fourie <jacques@fourie.ca>
2020-06-01 16:13:46 -0600

    Implemeneted linear N-Body physics engine

--- Player.js
+++ Player.js
@@ -1,16 +1,10 @@
-class Player {
+class Player extends physicsObject{
     constructor(x, y, z) {
+        super(true)
         this.spawn = new THREE.Vector3(x, y, z)
-        this.accel = new THREE.Vector3(0, 0, 0)
-        this.vel = new THREE.Vector3(0, 0, 0)
-        this.maxSpeed = 0.1
-        this.position = this.spawn
-        this.rotation = new THREE.Euler(0,0,0,'YXZ')
-        console.log(this.rotation)
-    }
-    movement(translate) {
-        this.position.add(
-            translate.applyEuler(this.rotation).multiplyScalar(this.maxSpeed)
-        )
+        this.position.copy(this.spawn)
+        this.rotation.copy(new THREE.Euler(0,0,0,'YXZ'))
+        this.mass = 20
+        this.radius = 1
     }
 }
--- main.js
+++ main.js
@@ -5,10 +5,10 @@ let renderer
 let player
 let ground
 let canvas
-let initTime = performance.now()
-let prevTime = performance.now()
 
 let clock = new THREE.Clock()
+
+let n = 10
 
 function init() {
     //Get html5 canvas and set pointer lock references
@@ -16,6 +16,7 @@ function init() {
 
     scene = new THREE.Scene()
 
+    // Initialize Camera
     camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
@@ -23,6 +24,7 @@ function init() {
         1000
     )
 
+    // Setup renderer
     renderer = new THREE.WebGLRenderer({
         canvas
     })
@@ -30,36 +32,69 @@ function init() {
     renderer.autoClearColor = false
 
     renderer.setSize(window.innerWidth, window.innerHeight)
-
-    world = new World()
+    // will need to add autoresize
 
     //Game stuff
-    player = new Player(2, 6, 8)
+    universe = new Universe()
+    planets = new Array(n)
+    for (let i = 0; i<n;i++){
+        let x = Math.floor(Math.random() * 200) - 100
+        let y = Math.floor(Math.random() * 200) - 100
+        let z = Math.floor(Math.random() * 200) - 100
+        console.log(x)
+        planets[i] = new Planet(5,100,new THREE.Vector3(x,y,z))
+    }
+    // x = 0.7494421910777922289898659 * -5
+    // y = 1.1501789857502275024030202 * -5
+    // planets[0] = new Planet(5, 10, new THREE.Vector3(-15, 0, 0))
+    // //planets[0].velocity.copy(new THREE.Vector3(x*-0.5,y*-0.5,0))
+    // planets[1] = new Planet(5, 10, new THREE.Vector3(0, 0, 0))
+    // //planets[1].velocity.copy(new THREE.Vector3(x,y,0))
+    // planets[2] = new Planet(5, 10, new THREE.Vector3(35, 0, 0))
+    // //planets[2].velocity.copy(new THREE.Vector3(x*-0.5,y*-0.5,0))
+    planets.forEach((planet) => {
+        planet.label = new Label()
+    })
+    player = new Player(0, 0, 50)
     controls = new Controls(player)
     document.addEventListener('mousemove', (e) => {
         controls.mouseMove(e)
     })
+    debugMenu = new Menu("Debug")
+    fps = debugMenu.addText("FPS:")
+    debugMenu.insert()
+}
+
+function update() {
+    camera.position.copy(player.position)
+    camera.quaternion.setFromEuler(player.rotation)
+    universe.updateSkybox(player.position)
 }
 
 function fixedUpdate() {
-    
     //update camera quarternion from players
-    //console.log(controls.movementVector)
-    player.movement(controls.movementVector)
-    camera.position.copy(player.position)
-    camera.quaternion.setFromEuler(player.rotation)
-    world.updateBackground(player.position)
+    physicsObject.update()
+    player.velocity.copy(controls.movementVector.applyEuler(player.rotation).multiplyScalar(1))
+    camera.updateMatrixWorld()
+    planets.forEach((planet) => {
+        planet.label.updateInner([`ID: ${planet.uuid}`,
+        `POS: ${planet.position.x.toFixed(2)} ${planet.position.y.toFixed(2)} ${planet.position.z.toFixed(2)}`,
+        `VEL: ${planet.velocity.x.toFixed(2)} ${planet.velocity.y.toFixed(2)} ${planet.velocity.z.toFixed(2)}`,
+        `ACC: ${planet.acceleration.x.toFixed(2)} ${planet.acceleration.y.toFixed(2)} ${planet.acceleration.z.toFixed(2)}`])
+        planet.label.updatePosition(planet.position)
+    })
 }
 
 function render() {
-    clock.getDelta()
-    world.render()
+    debugMenu.updateText(`FPS: ${(1/clock.getDelta()).toFixed(2)}`, fps)
+    universe.render()
     renderer.render(scene, camera)
 }
 
 init()
-setInterval(fixedUpdate, 5)
+setInterval(fixedUpdate, 1)
 
 renderer.setAnimationLoop(() => {
+    update()
     render()
 })
